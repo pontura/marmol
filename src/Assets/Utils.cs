b6b9aeafbad6d1fe2dcca884ce2857dd5cc965c5
Agustin Pontura <pontura@gmail.com>
2020-09-01 20:16:50 -0300

    Update Utils.cs

--- src/Assets/Utils.cs
+++ src/Assets/Utils.cs
@@ -1,8 +1,9 @@
-﻿ ﻿using UnityEngine;
- using System.Collections;
- using System.Collections.Generic;
- 
- public static class Utils {
+﻿﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+public static class Utils {
  
      public static void RemoveAllChildsIn(Transform container)
      {
@@ -89,5 +90,13 @@
         }
 
         return hashString.PadLeft(32, '0');
+    }
+    public static bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 1;
     }
 }
